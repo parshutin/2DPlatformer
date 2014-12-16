e53ff00517139bdedbb056a9871a42a7b66c5859
Murashko <Murashko@murashkopc>
2014-12-16 22:01:37 +0200

    Added project

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Scripts
+{
+	public class PlatformMover : MonoBehaviour
+	{
+		private void Awake()
+		{
+			rigidbody2D.velocity = new Vector2(-25, 0);
+		}
+	}
+}
